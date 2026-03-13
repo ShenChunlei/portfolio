676bd2492f4b2e361e1b21b19ec9a8d987b93f76
[dyad] <git@dyad.sh>
2026-03-13 09:00:16 +0800

    [dyad] 修复 Features 组件导入错误 - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,9 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import Features from '../components/Features';
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
@@ -13,7 +15,9 @@ const Index = () => {
       <Navbar />
       <main>
         <Hero />
-        <Features />
+        <About />
+        <Skills />
+        <Experience />
         <Projects />
         <Contact />
       </main>
