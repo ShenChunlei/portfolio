307dcf83101734ff2a5a63d3c44518196ba6d0f6
[dyad] <chsim.shen@gmail.com>
2026-03-13 14:36:20 +0800

    [dyad] 修复 CSS 导入路径错误 - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
-import './index.css';
+import './globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
